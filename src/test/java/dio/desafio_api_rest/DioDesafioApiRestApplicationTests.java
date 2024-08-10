f7ef4c552b657a650f54d1c711ecc28625cebecc
Thayssa <thayssamml@outlook.com>
2024-08-10 14:51:40 -0300

    Initial commit

---
+++ src/test/java/dio/desafio_api_rest/DioDesafioApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafio_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesafioApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
